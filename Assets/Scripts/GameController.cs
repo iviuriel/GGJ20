1d8e905bf7948fa06f63ad59139dd9f0e2df4cf9
Álvaro Gómez Harp <alvarogharo@gmail.com>
2020-01-31 23:17:06 +0100

    Cambiado todo a la raiz del repositorio

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    private readonly int TotalPopulation = 500;
+
+    public GameObject Person;
+    public GameObject[] People;
+    public int NumberOfPeople;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        People =  new GameObject[NumberOfPeople];
+        for(int i = 0; i < NumberOfPeople; i++) {
+            People[i] = Instantiate(Person, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int CalculateScore(int[] attributes1, int[] attributes2){
+        int result = 0;
+        for(int i = 0; i < 3; i++) {
+            result += attributes1[i] + attributes2[i];
+        }
+        return (result/30) * 10;
+    }
+}
---
+++ Assets/Scripts/PersonAtributes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonAtributes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private readonly int UpperRange = 5;
+    private readonly int LowerRange = -5;
+
+    public int CatDogs;
+    public int HamburgerPizza;
+    public int MountainBeach;
+
+    public int[] Attributes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CatDogs = Random.Range(UpperRange, LowerRange);
+        HamburgerPizza = Random.Range(UpperRange, LowerRange);
+        MountainBeach = Random.Range(UpperRange, LowerRange);
+
+        Attributes = new int[] {CatDogs, HamburgerPizza, MountainBeach};
+    }
+}
