d8605a3a413c0d4de27d971db13df8c67875ecb1
Álvaro Gómez Harp <alvarogharo@gmail.com>
2020-02-01 00:08:09 +0100

    Spawn areas feature

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,25 +6,30 @@ public class GameController : MonoBehaviour
 {
 
     private readonly int TotalPopulation = 500;
+    private readonly string SpawnAreaTag = "SpawnArea";
 
     public GameObject Person;
     public GameObject[] People;
     public int NumberOfPeople;
-    public int Test;
+    public GameObject[] SpawnAreas;
 
     // Start is called before the first frame update
     void Start()
     {
+        SpawnAreas = GameObject.FindGameObjectsWithTag(SpawnAreaTag);
         People =  new GameObject[NumberOfPeople];
+        
         for(int i = 0; i < NumberOfPeople; i++) {
-            People[i] = Instantiate(Person, new Vector3(0, 0, 0), Quaternion.identity);
+            int spawnAreaIndex = Random.Range(0, SpawnAreas.Length);
+            Vector2 areaCenter = SpawnAreas[spawnAreaIndex].transform.position;
+            float radius =  SpawnAreas[spawnAreaIndex].GetComponent<CircleCollider2D>().radius;
+            People[i] = Instantiate(Person, areaCenter + Random.insideUnitCircle * radius, Quaternion.identity);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Test = CalculateScore(People[3].GetComponent<PersonAttributes>().Attributes, People[2].GetComponent<PersonAttributes>().Attributes);
     }
 
     private int CalculateScore(int[] attributes1, int[] attributes2){
