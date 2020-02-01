47b8b4d4cfe85ee5b47a4a556ea323f17662445e
Álvaro Gómez Harp <alvarogharo@gmail.com>
2020-02-01 17:51:08 +0100

    Decor positioning script

---
+++ Assets/Scripts/Decor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Decor : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        position[2] = position[1];
+        this.transform.position = position;
+    }
+}
