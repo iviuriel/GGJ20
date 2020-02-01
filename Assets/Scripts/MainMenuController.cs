e39062a9d233bf25e50d20bc292312053ba95e45
Álvaro Gómez Harp <alvarogharo@gmail.com>
2020-02-01 02:58:34 +0100

    Added main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Animator PopUpAnimator; 
+
+    public void Play() {
+        SceneManager.LoadScene("GameController");
+    }
+
+    public void ShowHidePopUp() {
+        PopUpAnimator.SetTrigger("hidePopUp");
+    }
+
+    public void ResetPopulation() {
+
+    }
+}
